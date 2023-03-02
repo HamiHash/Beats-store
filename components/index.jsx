eaa382f4ea001dc14a8ad935cda46fa0f00f81df
HamiHash <hami.hash@yahoo.com>
2023-03-02 23:19:07 +0330

    initial

---
+++ components/index.jsx
@@ -0,0 +1,15 @@
+export { default as Footer } from "./Footer";
+export { default as Cart } from "./Cart";
+export { default as FooterBanner } from "./FooterBanner";
+export { default as Layout } from "./Layout";
+export { default as NavBar } from "./NavBar";
+export { default as Product } from "./Product";
+export { default as HeroBanner } from "./HeroBanner";
+
+import React from "react";
+
+const index = () => {
+  return <div>index</div>;
+};
+
+export default index;

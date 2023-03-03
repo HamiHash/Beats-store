e5b3aa245fff351931effb52a34a07a0ecbaf3ed
HamiHash <hami.hash@yahoo.com>
2023-03-04 01:16:44 +0330

    connected to sanity

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+import { client } from "../lib/client";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((products) => products)}
+        {products?.map((products) => products.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
